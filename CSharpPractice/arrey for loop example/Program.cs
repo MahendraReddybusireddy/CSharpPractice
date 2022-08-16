a43bfbd2d6ce719bc32d49f060a8d408d7bd5f73
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-16 16:17:50 +0530

    arrey for  loop example

---
+++ CSharpPractice/arrey for loop example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrey_for_loop_example
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i;
+            Console.WriteLine("Input 10 elements in the array:\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element - {0}:",i);  
+                arr[i] = 
+                    Convert.ToInt32(Console.ReadLine());    
+            }
+            Console.WriteLine("\nElements in arrey are:");
+            for(i =0; i < 10;i ++)
+            {
+                Console.Write("{0}", arr[i]);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
