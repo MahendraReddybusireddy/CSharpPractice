e993d561e007502e414069da5a9186c2488baa07
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-16 15:49:36 +0530

    arrey example in for loop

---
+++ CSharpPractice/arrey example in for loop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrey_example_in_for_loop
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] evenNums = { 2, 4, 6, 8, 10 };
+            for (int i = 0; i < evenNums.Length; i++) 
+            {
+                Console.WriteLine(evenNums[i]); 
+            }
+            for (int i = 0; i < evenNums.Length;i++)
+            evenNums[i] = evenNums[i] + 10;
+            {
+                Console.WriteLine("After Setting the values ");
+            }
+            for(int i = 0; i< evenNums.Length;i++)
+            {
+                Console.WriteLine(evenNums[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        
+
+    
+
