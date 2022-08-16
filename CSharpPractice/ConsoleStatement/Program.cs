f4ca6078858f4d2a4d339c1342953690dd204d41
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-16 17:11:10 +0530

    ConsoleStatement

---
+++ CSharpPractice/ConsoleStatement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStatement
+{
+    internal class Program
+    {
+        public void Show(out int a, out int b)
+        {
+            int square = 5;
+            a = square;
+            b = square;
+            a *= b;
+            b *= b;
+        }
+        static void Main(string[] args)
+        {
+            int val1 = 50, val2 = 100;
+            Program program = new Program();
+            Console.WriteLine("Valvue before passing\n val1 = " + val1 + "\n val2=" + val2);
+            program.Show(out val1, out val2);
+            Console.WriteLine("Value after passing\n val1 =" + val1 + "\nval2 = " + val2);
+            Console.ReadLine();
+        }
+    }
+}
