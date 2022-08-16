f4f984de924d23e792f71f9ffa5c9d939524e875
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-16 14:46:04 +0530

    implementwd PlusMinus

--- CSharpPractice/PlusMinusTest/Program.cs
+++ CSharpPractice/PlusMinusTest/Program.cs
@@ -8,8 +8,42 @@ namespace PlusMinusTest
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int[] numbers = new int[n];
+            for (int i = 0;i < n; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine().Trim());
+            }
+            plusMinus(numbers);
+            Console.ReadLine();
+        }
+        public static void plusMinus(int[]numbers)
+        {
+            int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+            foreach (int number in numbers)
+            {
+                if (number == 0) 
+                {
+                    zeroCount++;
+                }
+                else if(number>0)
+                {
+                    positiveCount++;
+                }
+                else
+                {
+                    negativeCount++;
+                }
+            }
+            double zeroNumberPercentage = (double)zeroCount / (double)numbers.Length;
+            double positiveNumberPercentage = (double)positiveCount / (double)numbers.Length;
+            double negativeNumberPercentage = (double)negativeCount / (double)numbers.Length;
+            Console.WriteLine(positiveNumberPercentage);
+            Console.WriteLine(negativeNumberPercentage);
+            Console.WriteLine(zeroNumberPercentage);
+
         }
     }
 }
