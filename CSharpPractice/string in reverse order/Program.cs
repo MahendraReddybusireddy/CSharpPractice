617efe8c4fbc4d06ef61606310a88c1a1a3cd54f
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-18 20:15:41 +0530

    string  in reverse order

---
+++ CSharpPractice/string in reverse order/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_in_reverse_order
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string str;
+            int I = 0;
+            Console.Write("Input the string :");
+            str = Console.ReadLine();
+            I = str.Length - 1;
+            Console.Write(" The characters of the string in reverse are : \n");
+            while (I >= 0)
+            {
+                Console.Write("{0}", str[1]);
+                I --;
+            }
+            Console.Write("\n\n");
+            Console.ReadLine();
+           
+        }
+    }
+}
