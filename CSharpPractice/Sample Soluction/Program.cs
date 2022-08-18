4eca731180cde10db0643a223ddafa11f6322804
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-18 19:20:49 +0530

    Sample Soluction

---
+++ CSharpPractice/Sample Soluction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Soluction
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string str;
+            int I = 0;
+            Console.Write("Input the string :");
+            str = Console.ReadLine();
+            foreach (char chr in str)
+            {
+                I += 1;
+            }
+            Console.Write("Length of the string is : {0}\n\n", 1);
+            Console.ReadLine();
+
+        }
+    }
+}
