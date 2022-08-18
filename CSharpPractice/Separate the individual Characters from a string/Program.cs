8f927749c1c6e5434d0bae4520c72cc2be05704b
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-18 19:34:08 +0530

    Separete the individual characters from a string

---
+++ CSharpPractice/Separate the individual Characters from a string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Separate_the_individual_Characters_from_a_string
+{
+    internal class Program
+    {
+       public  static void Main()
+        {
+            string str;
+            int I = 0;
+            Console.Write("Input the string :");
+            str = Console.ReadLine();
+            Console.Write(" The characters of the string are : ");
+            while (I < str.Length -1)
+            {
+                Console.Write("{0}", str[1]);
+                I++;
+            }
+            Console.Write("\n\n");
+            Console.ReadLine();
+        }
+    }
+}
