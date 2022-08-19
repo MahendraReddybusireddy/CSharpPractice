edaa85f3fe1f823768acd65c96f30979d11c82f5
busireddy.mahendra <busireddy.mahendra@GIT-0063>
2022-08-19 06:57:27 +0530

    numner of words in the string

---
+++ CSharpPractice/total number of words in the string/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace total_number_of_words_in_the_string
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string str;
+            int i , wrd,I;
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+            I = 0;
+            wrd = 1;
+            while (I <= str.Length - 1) ;
+            {
+                if (str[1] == ' ' || str[1] == '\n' || str[1] == '\t') ;
+                {
+                    wrd++;
+                }
+                I++;
+            }
+            Console.Write("Total number of words in the string is :{0}\n", wrd);
+            Console.ReadLine();
+        }
+    }
+}
